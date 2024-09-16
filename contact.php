b26994abf43e74eede24b42f2085a1044b55b283
Jochem Grootherder <J.grootherder@student.han.nl>
2024-09-16 10:19:32 +0200

    Generiek werkend. Oude code moet nog weg #21

--- contact.php
+++ contact.php
@@ -1,20 +1,21 @@
 <?php
-define ("GENDERS" , array('Mr.' => 'Mr.', 'Mrs.' => 'Mrs.', 'Other' => 'Other'));
-define ("COMMUNICATION_PREFERENCES", array('email' => 'Email', 'phone' => 'Phone', 'mail' => 'Mail'));
+include "defines.php";
 
 function getContactFormData()
 {
+    global $COMMUNICATION_PREFERENCES;
+    global $GENDERS;
     return
     [
-        'gender' => ['label' => 'Gender', 'type' => 'select', 'placeholder' => 'Mr.','options' => GENDERS, 'validations' => ["notEmpty", "validOption"]],
+        'gender' => ['label' => 'Gender', 'type' => 'select', 'placeholder' => 'Mr.','options' => $GENDERS, 'validations' => ["notEmpty", "validOption"]],
         'name'  => ['label' => 'Full Name', 'type' => 'text', 'placeholder' => 'Full name', 'validations' => ["notEmpty", "onlyCharacters"]],
-        'email' => ['label' => 'Email', 'type' => 'text', 'placeholder' => 'Example@example.com', 'validations' => ["notEmptyIf:communication:email", "validEmail"]],
-        'phonenumber' => ['label' => 'Phone Number', 'type' => 'text', 'placeholder' => '0612345678', 'validations' => ["notEmptyIf:communication:phone", "validPhoneNumber"]],
-        'streetname' => ['label' => 'Streetname', 'type' => 'text', 'placeholder' => 'Streetname', 'validations' => ["notEmptyIf:communication:mail", "onlyCharacters"]],
-        'housenumber' => ['label' => 'Nr + addition', 'type' => 'text', 'placeholder' => '123A01', 'validations' => ["notEmptyIf:communication:mail", "validHouseNumber"]],
-        'zipcode' => ['label' => 'Zipcode', 'type' => 'text', 'placeholder' => '1234AB', 'validations' => ["notEmptyIf:communication:mail", "validZipcode"]],
-        'city' => ['label' => 'City', 'type' => 'text', 'placeholder' => 'City', 'validations' => ["notEmptyIf:communication:mail", "onlyCharacters"]],
-        'communicationPreference' => ['label' => 'Communication', 'type' => 'radio', 'placeholder' => 'Email', 'options' => COMMUNICATION_PREFERENCES, 'validations' => ["notEmpty", "validOption"]],
+        'Email' => ['label' => 'Email', 'type' => 'text', 'placeholder' => 'Example@example.com', 'validations' => ["notEmptyIf:communication:Email", "validEmail"]],
+        'Phonenumber' => ['label' => 'Phone Number', 'type' => 'text', 'placeholder' => '0612345678', 'validations' => ["notEmptyIf:communication:Phone", "validPhoneNumber"]],
+        'streetname' => ['label' => 'Streetname', 'type' => 'text', 'placeholder' => 'Streetname', 'validations' => ["notEmptyIf:communication:Mail", "onlyCharacters"]],
+        'housenumber' => ['label' => 'Nr + addition', 'type' => 'text', 'placeholder' => '123A01', 'validations' => ["notEmptyIf:communication:Mail", "validHouseNumber"]],
+        'zipcode' => ['label' => 'Zipcode', 'type' => 'text', 'placeholder' => '1234AB', 'validations' => ["notEmptyIf:communication:Mail", "validZipcode"]],
+        'city' => ['label' => 'City', 'type' => 'text', 'placeholder' => 'City', 'validations' => ["notEmptyIf:communication:Mail", "onlyCharacters"]],
+        'communicationPreference' => ['label' => 'Communication', 'type' => 'radio', 'placeholder' => 'Email', 'options' => $COMMUNICATION_PREFERENCES, 'validations' => ["notEmpty", "validOption"]],
         'message' => ['label' => 'Message', 'type' => 'textarea', 'placeholder' => 'Message', 'validations' => ["notEmpty"]]
     ];
 }
@@ -117,17 +118,17 @@ function getContactFormData()
             }
             break;
 
-        case 'email':
+        case 'Email':
             if(!empty($value))
             {
                 if(!filter_var($value, FILTER_VALIDATE_EMAIL))
                 {
-                    $error = "Invalid email format. Expected: example@example.com";
+                    $error = "Invalid Email format. Expected: example@example.com";
                 }  
             }
             break;
         
-        case 'phonenumber':
+        case 'Phonenumber':
         
             //0612345678
             //OR
@@ -228,9 +229,9 @@ function getContactFormData()
             {
                 $error = $key." is required";
             }
-            else if(!($value == 'email' || $value == 'phone' || $value =='mail'))
+            else if(!($value == 'Email' || $value == 'Phone' || $value =='Mail'))
             {
-                $error = $key." must be either email, phone or mail";
+                $error = $key." must be either Email, Phone or Mail";
             }
             break;
             
@@ -270,6 +271,7 @@ function getContactFormData()
         {
             $formResult = validateField($key, $metaData, $formResults);
         }
+        return $formResults;
     }
 
 
@@ -278,13 +280,13 @@ function getContactFormData()
     
     function showBody()
     {
-        $gender = $name = $email = $phonenumber = $streetname = 
+        $gender = $name = $Email = $Phonenumber = $streetname = 
         $housenumber = $zipcode = $city = $communicationPreference = $message = "";
         
-        $genderErr = $nameErr = $emailErr = $phonenumberErr = $streetnameErr = 
+        $genderErr = $nameErr = $EmailErr = $PhonenumberErr = $streetnameErr = 
         $housenumberErr = $zipcodeErr = $cityErr = $communicationPreferenceErr = $messageErr = "";
         
-        $emailPreference = $phonePreference = $mailPreference = false;
+        $EmailPreference = $PhonePreference = $mailPreference = false;
         $validInput = false;
         $requiredInputFilled = false;
         $valid = false;
@@ -297,14 +299,14 @@ function getContactFormData()
             <!-- Form Name -->
             <legend>Contact us</legend>';
 
-            getDataFromPost(getContactFormData());
+        $formResults = getDataFromPost(getContactFormData());
 
         foreach(getContactFormData() as $key => $metaData)
         {
-            $error = '';
-            $value = getContactPostVar($key, $error);
+            //$error = '';
+            //$value = getContactPostVar($key, $error);
             //echo "value: " . $value. " - error: ".$error."<br>";
-            $formResult = ['value' => $value, 'error' => $error];
+            $formResult = ['value' => $formResults[$key]['value'], 'error' => $formResults[$key]['error']];
             showFormField($key, $metaData, $formResult);
         }
         echo'
@@ -320,8 +322,8 @@ function getContactFormData()
         // validate the 'POST' data
         $gender = getContactPostVar('gender', $genderErr);
         $name = getContactPostVar('name', $nameErr);
-        $email = getContactPostVar('email', $emailErr);
-        $phonenumber = getContactPostVar('phonenumber', $phonenumberErr);
+        $Email = getContactPostVar('Email', $EmailErr);
+        $Phonenumber = getContactPostVar('Phonenumber', $PhonenumberErr);
         $streetname = getContactPostVar('streetname', $streetnameErr);
         $housenumber = getContactPostVar('housenumber', $housenumberErr);
         $zipcode = getContactPostVar('zipcode', $zipcodeErr);
@@ -332,23 +334,23 @@ function getContactFormData()
 
         switch($communicationPreference)
         {
-            case 'email':
-                if(empty($email))
+            case 'Email':
+                if(empty($Email))
                 {
-                    $emailErr = 'email is required';
+                    $EmailErr = 'Email is required';
                 }
-                $requiredInputFilled = !empty($email);
-                $validInput = empty($emailErr);
+                $requiredInputFilled = !empty($Email);
+                $validInput = empty($EmailErr);
                 break;
-            case 'phone':
-                if(empty($phonenumber))
+            case 'Phone':
+                if(empty($Phonenumber))
                 {
-                    $phonenumberErr = 'phone number is required';
+                    $PhonenumberErr = 'Phone number is required';
                 }
-                $requiredInputFilled = !empty($phonenumber);
-                $validInput = empty($phonenumberErr);
+                $requiredInputFilled = !empty($Phonenumber);
+                $validInput = empty($PhonenumberErr);
                 break;
-            case 'mail':
+            case 'Mail':
                 if(empty($streetname))
                 {
                     $streetnameErr = 'streetname is required';
@@ -372,7 +374,7 @@ function getContactFormData()
                     $requiredInputFilled = false;
                 break;
         }
-        $validInput = empty($genderErr) && empty($nameErr) && empty($emailErr) && empty($phonenumberErr) && 
+        $validInput = empty($genderErr) && empty($nameErr) && empty($EmailErr) && empty($PhonenumberErr) && 
                     empty($streetnameErr) && empty($housenumberErr) && empty($zipcodeErr) && empty($cityErr) &&
                     empty($communicationPreferenceErr) && empty($messageErr);
         
@@ -436,21 +438,21 @@ function getContactFormData()
 
             <!-- Text input-->
             <div class="form-group">
-                <label class="control-label" for="email">Email</label>
-                <input name="email" type="text" placeholder="Example@email.com" class="form-control" value="<?php echo $email?>">
+                <label class="control-label" for="Email">Email</label>
+                <input name="Email" type="text" placeholder="Example@Email.com" class="form-control" value="<?php echo $Email?>">
 
-                <?php if(!empty($emailErr)){?>
-                    <span class="error">* <?php echo $emailErr; ?></span>
+                <?php if(!empty($EmailErr)){?>
+                    <span class="error">* <?php echo $EmailErr; ?></span>
                 <?php }?>
             </div>
 
             <!-- Text input-->
             <div class="form-group">
-                <label class="control-label" for="phonenumber">Phone number</label>
-                <input name="phonenumber" type="text" placeholder="+31612345678" class="form-control"
-                 value="<?php echo $phonenumber?>">
-                <?php if(!empty($phonenumberErr)){?>
-                    <span class="error">* <?php echo $phonenumberErr; ?></span>
+                <label class="control-label" for="Phonenumber">Phone number</label>
+                <input name="Phonenumber" type="text" placeholder="+31612345678" class="form-control"
+                 value="<?php echo $Phonenumber?>">
+                <?php if(!empty($PhonenumberErr)){?>
+                    <span class="error">* <?php echo $PhonenumberErr; ?></span>
                 <?php }?>
 
             </div>
@@ -499,21 +501,21 @@ function getContactFormData()
             <div class="form-group">
                 <label class="control-label" for="communicationPreference">Communication</label>
                 <label for="communicationPreference-0">
-                    <input type="radio" name="communicationPreference" value="email"
+                    <input type="radio" name="communicationPreference" value="Email"
                         checked="checked">
                     Email
                 </label>
                 <div class="radio">
                     <label for="communicationPreference-1">
-                        <input type="radio" name="communicationPreference" value="phone"
-                        <?php if($communicationPreference == "phone"){?>checked="checked"<?php }?>>
+                        <input type="radio" name="communicationPreference" value="Phone"
+                        <?php if($communicationPreference == "Phone"){?>checked="checked"<?php }?>>
                         Phone
                     </label>
                 </div>
                 <div class="radio">
                     <label for="communicationPreference-2">
-                        <input type="radio" name="communicationPreference" value="mail"
-                        <?php if($communicationPreference == "mail"){?>checked="checked"<?php }?>>
+                        <input type="radio" name="communicationPreference" value="Mail"
+                        <?php if($communicationPreference == "Mail"){?>checked="checked"<?php }?>>
                         Mail
                     </label>
                 </div>
@@ -549,8 +551,8 @@ function getContactFormData()
         Thank you for your message, we will be in contact soon. </br>
         Your details are: <br>
         Name: <?php echo $name?><br>
-        Email: <?php echo $email?><br>
-        Phone number: <?php echo $phonenumber?><br>
+        Email: <?php echo $Email?><br>
+        Phone number: <?php echo $Phonenumber?><br>
         Streetname: <?php echo $streetname?><br>
         House number + addition: <?php echo $housenumber?><br>
         Zipcode: <?php echo $zipcode?><br>
---
+++ defines.php
@@ -0,0 +1,8 @@
+<?php
+global $GENDERS;
+$GENDERS = array('Mr.' => 'Mr.', 'Mrs.' => 'Mrs.', 'Other' => 'Other');
+global $COMMUNICATION_PREFERENCES;
+$COMMUNICATION_PREFERENCES= array('Email' => 'Email', 'Phone' => 'Phone', 'Mail' => 'Mail');
+//define ("GENDERS" , array('Mr.' => 'Mr.', 'Mrs.' => 'Mrs.', 'Other' => 'Other'));
+//define ("COMMUNICATION_PREFERENCES", array('email' => 'Email', 'phone' => 'Phone', 'mail' => 'Mail'));
+?>
